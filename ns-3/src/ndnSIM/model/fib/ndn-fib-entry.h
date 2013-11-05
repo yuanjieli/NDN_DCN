4a8e2fd68fc972d32841a394948151de141deabf
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-11-04 18:45:02 -0800

    Iterative approach without NACK or marked scheme

--- ns-3/src/ndnSIM/model/fib/ndn-fib-entry.h
+++ ns-3/src/ndnSIM/model/fib/ndn-fib-entry.h
@@ -268,8 +268,11 @@ public:
   									 /(double)(m_data_ce_old+1)-m_nack_old;*/
   									 
   	//cooperation/price_of_competition
-  	m_sharing_metric = (m_data_in_old+1)*(m_data_in_old+1)*(m_data_in_old+1)
-  									 /(double)((m_data_ce_old+1)*(m_nack_old+1));
+  	/*m_sharing_metric = (m_data_in_old+1)*(m_data_in_old+1)*(m_data_in_old+1)
+  									 /(double)((m_data_ce_old+1)*(m_nack_old+1));*/
+  	
+  	//iterative approach without NACK count
+  	m_sharing_metirc = m_data_in_old;
   	
   	m_data_in = 0;
   	m_data_ce = 0;
