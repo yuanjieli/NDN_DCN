af3132ff8c1e23bb27b28764b3a65378b50f68d6
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-11-21 20:13:41 -0800

    debug

--- ns-3/scratch/ndncc-cooperation.cc
+++ ns-3/scratch/ndncc-cooperation.cc
@@ -91,14 +91,14 @@ main (int argc, char *argv[])
   ndn::StackHelper::AddRoute ("S2","/prefix1","S4",1);	
   //S3
   ndn::StackHelper::AddRoute ("S3","/prefix1","S4",1);	
-  ndn::StackHelper::AddRoute ("S2","/prefix3","S4",1);
+  ndn::StackHelper::AddRoute ("S3","/prefix3","S4",1);
   	
   ndn::GlobalRoutingHelper ndnGlobalRoutingHelper;
   ndnGlobalRoutingHelper.InstallAll ();
-  /*ndnGlobalRoutingHelper.AddOrigins ("/prefix1", Names::Find<Node> ("S4"));
+  ndnGlobalRoutingHelper.AddOrigins ("/prefix1", Names::Find<Node> ("S4"));
   ndnGlobalRoutingHelper.AddOrigins ("/prefix2", Names::Find<Node> ("S2"));
   ndnGlobalRoutingHelper.AddOrigins ("/prefix3", Names::Find<Node> ("S4"));
-  ndnGlobalRoutingHelper.CalculateAllPossibleRoutes ();*/
+  //ndnGlobalRoutingHelper.CalculateAllPossibleRoutes ();
   ndnGlobalRoutingHelper.CalculateFIB2 ();			
   	
   
