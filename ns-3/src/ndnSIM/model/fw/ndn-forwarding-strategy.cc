9f142571974342663ab90ef7ccc6b947ec0cf689
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-11-23 10:11:56 -0800

    debug

--- ns-3/src/ndnSIM/model/fw/ndn-forwarding-strategy.cc
+++ ns-3/src/ndnSIM/model/fw/ndn-forwarding-strategy.cc
@@ -171,6 +171,7 @@ ForwardingStrategy::OnInterest (Ptr<Face> inFace,
 
   if (isDuplicated)
     {
+    	NS_LOG_UNCOND("Loop here!");
       DidReceiveDuplicateInterest (inFace, header, origPacket, pitEntry);
       return;
     }
