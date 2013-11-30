40a9b7e2b57747a5734e1d75d75cbe4e8c9249ca
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-11-29 17:23:38 -0800

    debug

--- ns-3/src/ndnSIM/apps/ndn-consumer-om.cc
+++ ns-3/src/ndnSIM/apps/ndn-consumer-om.cc
@@ -149,7 +149,7 @@ ConsumerOm::OnNack (const Ptr<const Interest> &interest, Ptr<Packet> packet)
 	Consumer::OnNack (interest, packet);
 		
 	//rule out nacks with different prefixes
-	std::list<std::string>::iterator rhs = interest->GetNamePtr()->begin();
+	std::list<std::string>::iterator rhs = interest->GetName().begin();
 	bool match = true;
 	for(std::list<std::string>::iterator it = m_interestName.begin();
 			it != m_interestName.end(); it++)
