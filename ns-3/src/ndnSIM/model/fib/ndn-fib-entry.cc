5b09b63677961888694be5d30f01f99017c2203f
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-11-29 17:37:10 -0800

    debug

--- ns-3/src/ndnSIM/model/fib/ndn-fib-entry.cc
+++ ns-3/src/ndnSIM/model/fib/ndn-fib-entry.cc
@@ -195,7 +195,8 @@ Entry::ResetCount()
 	    								<<" metric="<<face->GetFraction()
 	    								//<<" NACK="<<face->GetNack()
 	    								//<<" Data_in="<<face->GetDataIn()
-	    								//<<" Data_CE="<<face->GetDataCE());
+	    								//<<" Data_CE="<<face->GetDataCE()
+	    								);
       m_faces.modify (face,
                       ll::bind (&FaceMetric::ResetCounter, ll::_1));
     }
