00f1726ef875e50882d534cecc2e7eba60aa3150
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-01 11:54:17 -0800

    debug

--- ns-3/src/ndnSIM/model/fib/ndn-fib-entry.cc
+++ ns-3/src/ndnSIM/model/fib/ndn-fib-entry.cc
@@ -265,8 +265,8 @@ Entry::ResetCount()
     }
   //q_var /= facecount;
   q_var = sqrt(q_var/facecount);
-  K = K_bound*tanh(q_var/q_mean/5);  
-  //K = K_bound*tanh(q_var/q_mean); 
+  //K = K_bound*tanh(q_var/q_mean/5);  
+  K = K_bound*tanh(q_var/q_mean); 
   
   for (FaceMetricByFace::type::iterator face = m_faces.begin ();
        face != m_faces.end ();
