888267c0bb58b7586ad88f9bd81d4c99106b5025
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-02 10:22:46 -0800

    debug

--- ns-3/src/ndnSIM/apps/ndn-consumer-om.cc
+++ ns-3/src/ndnSIM/apps/ndn-consumer-om.cc
@@ -175,7 +175,7 @@ ConsumerOm::OnNack (const Ptr<const Interest> &interest, Ptr<Packet> packet)
 		if(interest->GetIntraSharing()==0)
 			m_limit = m_limit - m_beta;
 		else
-			m_limit = m_limit - m_beta*100;//*(double)(interest->GetIntraSharing())/100.0;  
+			m_limit = m_limit - m_beta*200;//*(double)(interest->GetIntraSharing())/100.0;  
 		if (m_limit <= m_initLimit)		//we need to avoid non-sense interest limit
 			m_limit = m_initLimit;	
 			
