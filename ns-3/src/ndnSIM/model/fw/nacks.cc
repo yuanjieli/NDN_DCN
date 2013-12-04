f7ab61d4aefe3783441afb2bb1fbf05f898f2a15
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-03 16:29:30 -0800

    debug

--- ns-3/src/ndnSIM/model/fw/nacks.cc
+++ ns-3/src/ndnSIM/model/fw/nacks.cc
@@ -218,10 +218,10 @@ Nacks::DidExhaustForwardingOptions (Ptr<Face> inFace,
       		//if inFace is not an application face, we may have intra-sharing problem
       		if(!ignore && DynamicCast<AppFace>(inFace)==0)
       		{
-      			/*if(inFace->GetNode()->GetId()==0)
+      			if(inFace->GetNode()->GetId()==0)
       				NS_LOG_UNCOND("Extra NACK from face="<<inFace->GetId()
       										<<" fraction="<<100-record->GetFraction()
-      										<<" to"<<app->GetFace()->GetId());*/
+      										<<" to"<<app->GetFace()->GetId());
 	      		nackHeader->SetIntraSharing(100-record->GetFraction());
 	      		app->OnNack(nackHeader, origPacket->Copy());
 	      	}
