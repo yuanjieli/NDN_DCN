3347cfb58297f1ea1f6779ab709aedd28467290c
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-03 22:23:11 -0800

    debug

--- ns-3/src/ndnSIM/apps/ndn-consumer-om.cc
+++ ns-3/src/ndnSIM/apps/ndn-consumer-om.cc
@@ -136,13 +136,11 @@ ConsumerOm::OnContentObject (const Ptr<const ContentObject> &contentObject,
 {
   Consumer::OnContentObject (contentObject, payload); // tracing inside
   //update interest limit
-  //if(contentObject->GetCE()!=2)	//not a local cache hit
+  if(contentObject->GetCE()!=2)	//not a local cache hit
   	m_limit = m_limit + m_alpha/m_limit;	//here we choose parameter such that the convergence time is similar to TCP
-  //else	//local hit, send next requests immediately
-  if(contentObject->GetCE()==2)	
-  	//SendPacket();
-  	Simulator::Schedule (Seconds (0.0),
-                         &Consumer::SendPacket, this);
+  else	//local hit, send next requests immediately
+  	SendPacket();
+  	
   
   m_data_count++;	
   /*if(m_sendEvent.IsRunning())
