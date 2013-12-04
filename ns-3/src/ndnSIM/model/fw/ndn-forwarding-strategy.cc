db3bbbee6f99f6978de0c7fb7f1c971266e886a9
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-03 20:13:53 -0800

    debug

--- ns-3/src/ndnSIM/model/fw/ndn-forwarding-strategy.cc
+++ ns-3/src/ndnSIM/model/fw/ndn-forwarding-strategy.cc
@@ -158,6 +158,7 @@ ForwardingStrategy::OnInterest (Ptr<Face> inFace,
         }
       else
         {
+        	NS_LOG_UNCOND("Siliently drop Interest?");
           return;
         }
     }
@@ -211,6 +212,7 @@ ForwardingStrategy::OnInterest (Ptr<Face> inFace,
       m_dropInterests (header, inFace);
 			
       DidSuppressSimilarInterest (inFace, header, origPacket, pitEntry);
+      NS_LOG_UNCOND("Suppress interests?");
       return;
     }
 
