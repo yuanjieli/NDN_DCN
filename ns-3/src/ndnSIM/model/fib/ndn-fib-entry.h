1c36855fa75cccaf805177befdcb944f0c3b0ce1
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-03 22:03:21 -0800

    debug

--- ns-3/src/ndnSIM/model/fib/ndn-fib-entry.h
+++ ns-3/src/ndnSIM/model/fib/ndn-fib-entry.h
@@ -79,11 +79,10 @@ public:
     , m_data_ce (0) 
     , m_nack_old (0)
     , m_data_in_old (0)
-    , m_data_ce_old (0)
-    , m_interest_count (0)
+    , m_data_ce_old (0)   
     , m_fraction (1) //initially arbitrary large number. Will be updated later
     , m_sharing_metric (1)
-    
+    , m_interest_count (0)
   { }
  
   /**
