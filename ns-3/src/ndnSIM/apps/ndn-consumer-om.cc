702cf5479cafcb58e6935e7fc2c28c2091e89e3c
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-10 20:14:50 -0800

    debug

--- ns-3/src/ndnSIM/apps/ndn-consumer-om.cc
+++ ns-3/src/ndnSIM/apps/ndn-consumer-om.cc
@@ -158,7 +158,7 @@ void
 ConsumerOm::OnNack (const Ptr<const Interest> &interest, Ptr<Packet> packet)
 {
 	Consumer::OnNack (interest, packet);
-			
+	NS_LOG_UNCOND("Test OnNack");		
 	//update interest limit
 	if(interest->GetNack()==Interest::NACK_GIVEUP_PIT)	//NOT NACK_CONGESTION
 	//if(interest->GetNack()==Interest::NACK_CONGESTION)
