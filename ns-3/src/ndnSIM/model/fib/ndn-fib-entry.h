8ff3723ab16155a5d4a6991a5f09c8ba8cdabcc8
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-11 12:23:45 -0800

    debug

--- ns-3/src/ndnSIM/model/fib/ndn-fib-entry.h
+++ ns-3/src/ndnSIM/model/fib/ndn-fib-entry.h
@@ -40,9 +40,9 @@
 #include <boost/multi_index/mem_fun.hpp>
 
 //parameters for weight update
-#define UPDATE_INTERVAL 1
+#define UPDATE_INTERVAL 10
 #define SHOW_RATE_INTERVAL 1
-#define ALPHA 1/8.0	//smoothed counter
+#define ALPHA 1//1/8.0	//smoothed counter
 
 namespace ns3 {
 namespace ndn {
