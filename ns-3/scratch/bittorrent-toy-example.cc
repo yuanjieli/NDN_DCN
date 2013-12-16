34a0bcd337f7f3575d7f0b42bd3485549d90bc12
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-16 15:51:14 -0800

    debug

--- ns-3/scratch/bittorrent-toy-example.cc
+++ ns-3/scratch/bittorrent-toy-example.cc
@@ -70,7 +70,7 @@ main (int argc, char *argv[])
   Names::Find<Node> ("S4")->AddApplication (bitTorrentTracker);	
   
   // 2) Load a torrent file via the tracker application
-  Ptr<Torrent> sharedTorrent = bitTorrentTracker->AddTorrent ("input/bittorrent/torrent-data", "input/bittorrent/torrent-data/100MB-full.dat.torrent");
+  Ptr<Torrent> sharedTorrent = bitTorrentTracker->AddTorrent ("input/bittorrent/torrent-data", "input/bittorrent/torrent-data/10MB-full.dat.torrent");
   
   // 3) Install BitTorrentClient applications on the desired number of nodes
   ApplicationContainer bitTorrentClients;
