7ffa149daaeed66c00582a521ee3393dabc79525
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-19 16:06:49 -0800

    debug

--- BCube/ns-3/src/ndnSIM/helper/ndn-bcube-routing-helper.cc
+++ BCube/ns-3/src/ndnSIM/helper/ndn-bcube-routing-helper.cc
@@ -380,8 +380,8 @@ BCubeRoutingHelper::CalculateBCubeRoutes(uint32_t m_n, uint32_t m_k)
 					std::string C_name = Names::FindName(C);
 					for(size_t j = 0; j < m_n-1; j ++)
 					{
-						C_name[dim] = '0' + (C[dim]-'0'+1)%m_n;
-						C = Named::Find<Node>(C_name);
+						C_name[dim] = '0' + (C_name[dim]-'0'+1)%m_n;
+						C = Names::Find<Node>(C_name);
 						TreeLink.push_back(std::make_pair(B, C));
 						B = C;
 					}
