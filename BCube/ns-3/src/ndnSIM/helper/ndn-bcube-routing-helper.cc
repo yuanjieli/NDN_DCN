d268c73285cc18d24f5c83f478e44894fa01784d
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-19 19:53:46 -0800

    debug

--- BCube/ns-3/src/ndnSIM/helper/ndn-bcube-routing-helper.cc
+++ BCube/ns-3/src/ndnSIM/helper/ndn-bcube-routing-helper.cc
@@ -422,6 +422,7 @@ BCubeRoutingHelper::CalculateBCubeRoutes(uint32_t m_n, uint32_t m_k)
 			for(uint32_t i = 0; i != nserver; i++)
 			{
 				std::string s_name = GetBCubeId(i, m_n);
+				NS_LOG_UNCOND("s_name="<<s_name);
 				if(s_name[level+1]!=src_name[level+1] || s_name == src_name)
 					continue;
 				Ptr<Node> S = Names::Find<Node>(s_name);
