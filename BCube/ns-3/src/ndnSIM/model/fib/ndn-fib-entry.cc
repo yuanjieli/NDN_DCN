9ea9c86869937e5c2d6a001776527a3dcaf01551
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-21 15:54:20 -0800

    debug

--- BCube/ns-3/src/ndnSIM/model/fib/ndn-fib-entry.cc
+++ BCube/ns-3/src/ndnSIM/model/fib/ndn-fib-entry.cc
@@ -261,8 +261,8 @@ Entry::ResetCount()
     	}							
     }
   q_var = sqrt(q_var)/facecount;
-  //K = K_bound*tanh(q_var/(1+q_mean)/5);  
-  K = K_bound*tanh(q_var/(1+q_mean)/2);  
+  K = K_bound*tanh(q_var/(1+q_mean)/5);  
+  //K = K_bound*tanh(q_var/(1+q_mean)/2);  
   
   for (FaceMetricByFace::type::iterator face = m_faces.begin ();
        face != m_faces.end ();
