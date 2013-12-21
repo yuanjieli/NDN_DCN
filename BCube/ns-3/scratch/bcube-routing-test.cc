80add24e5adcb248e549b69a9537c4ee63eea1c0
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-21 11:45:31 -0800

    debug

--- BCube/ns-3/scratch/bcube-routing-test.cc
+++ BCube/ns-3/scratch/bcube-routing-test.cc
@@ -82,7 +82,7 @@ main (int argc, char *argv[])
   ndn::AppHelper consumerHelper ("ns3::ndn::ConsumerOm");
   ApplicationContainer consumers;
   consumerHelper.SetPrefix ("/prefix");
-  consumers = consumerHelper.Install (Names::Find<Node>("S10")); 
+  consumers = consumerHelper.Install (Names::Find<Node>("S33")); 
   consumers.Start (Seconds (0));	
   consumers.Stop (Seconds (simulation_time));
   
