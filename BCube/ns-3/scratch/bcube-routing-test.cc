052ef61ba38b8466159a13c371141dd3f6115647
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-21 17:27:03 -0800

    debug

--- BCube/ns-3/scratch/bcube-routing-test.cc
+++ BCube/ns-3/scratch/bcube-routing-test.cc
@@ -98,8 +98,8 @@ main (int argc, char *argv[])
   		if(i==0 && j==0)continue;
   		consumerHelper.SetPrefix ("/prefix");
   		std::string str = 'S';
-  		str += '0'+i;
-  		str += '0'+j;
+  		str += "0"+i;
+  		str += "0"+j;
   		consumers = consumerHelper.Install (Names::Find<Node>(str)); 
   		consumers.Start (Seconds (0));	
   		consumers.Stop (Seconds (simulation_time));
