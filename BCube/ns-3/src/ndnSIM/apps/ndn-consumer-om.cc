4abea5fb31879b7ace40cdc419c97887a37191a6
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-21 23:17:08 -0800

    deug

--- BCube/ns-3/src/ndnSIM/apps/ndn-consumer-om.cc
+++ BCube/ns-3/src/ndnSIM/apps/ndn-consumer-om.cc
@@ -148,15 +148,15 @@ ConsumerOm::OnContentObject (const Ptr<const ContentObject> &contentObject,
 	
   Consumer::OnContentObject (contentObject, payload); // tracing inside
   //update interest limit
-  if(contentObject->GetCE()!=2)	//not a local cache hit
+  //if(contentObject->GetCE()!=2)	//not a local cache hit
   {
   	m_limit = m_limit + m_alpha/m_limit;	//here we choose parameter such that the convergence time is similar to TCP
   	m_alpha += 1/m_limit;
   	if(m_alpha>m_alpha_max)
   		m_alpha = m_alpha_max;
   }
-  else	//local hit, send next requests immediately
-  	SendPacket();
+  //else	//local hit, send next requests immediately
+  	//SendPacket();
   	//SendRandomPacket();
   	
   
