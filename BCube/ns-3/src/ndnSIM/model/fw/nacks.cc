e2f9a868d6367340a411f604055fac3bf063bcf0
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-21 19:11:45 -0800

    debug

--- BCube/ns-3/src/ndnSIM/model/fw/nacks.cc
+++ BCube/ns-3/src/ndnSIM/model/fw/nacks.cc
@@ -198,8 +198,8 @@ Nacks::DidExhaustForwardingOptions (Ptr<Face> inFace,
 		  tag.SetNextHop(incoming.m_localport);
 		  target->AddPacketTag(tag);
           //incoming.m_face->Send (packet->Copy ());	//by Felix: NACK is multicasted!!!
-		  NS_LOG_UNCOND(Names::FindName(inFace->GetNode())
-		  			 <<" wants to send nack to "<<(uint32_t)incoming.m_localport);
+		  /*NS_LOG_UNCOND(Names::FindName(inFace->GetNode())
+		  			 <<" wants to send nack to "<<(uint32_t)incoming.m_localport);*/
 		  incoming.m_face->Send(target);
 					
           m_outNacks (nackHeader, incoming.m_face);
