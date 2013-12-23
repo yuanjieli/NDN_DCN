d3f36b03289d1eff61941bbb5111de55428fd314
Yuanjie Li <mathchinese@yahoo.com.cn>
2013-12-23 14:36:21 -0800

    debug

--- BCube/ns-3/src/ndnSIM/model/fw/best-cc.cc
+++ BCube/ns-3/src/ndnSIM/model/fw/best-cc.cc
@@ -207,6 +207,7 @@ BestCC::DoPropagateInterest (Ptr<Face> inFace,
 				 	}
 			 	}
 			 	NS_ASSERT(optimalFace != 0);
+			 	NS_LOG_UNCOND("");
 		 	}
 		 	else	//There SHOULD be a source routing tag
 		 	{
